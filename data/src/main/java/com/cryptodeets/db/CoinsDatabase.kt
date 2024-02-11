7551800a3dbdfb4e7d07a024340ed4932c053851
Kosgkagiannis <kosgkagiannis@gmail.com>
2024-02-11 22:03:47 +0100

    Remove unused auto migration

--- data/src/main/java/com/cryptodeets/db/CoinsDatabase.kt
+++ data/src/main/java/com/cryptodeets/db/CoinsDatabase.kt
@@ -20,9 +20,7 @@ import com.cryptodeets.data.features.topcoins.local.models.TopCoinEntity
     version = 4,
     exportSchema = true,
     autoMigrations = [
-        AutoMigration (from = 1, to = 2),
-        AutoMigration (from = 2, to = 3),
-        AutoMigration(from = 3, to = 4)
+        AutoMigration (from = 1, to = 2)
     ]
 )
 abstract class CoinsDatabase : RoomDatabase() {
